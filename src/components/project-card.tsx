31708286628e4b60c2b7b30bdf8ef2b5db64be39
unknown <waqar.christiano@gmail.com>
2025-06-22 11:33:12 -0500

    final 12

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -68,7 +68,7 @@ export function ProjectCard({
               alt={title}
               width={500}
               height={300}
-              className="h-40 w-full overflow-hidden object-cover object-top"
+              className="h-40 w-full overflow-hidden object-contain object-top"
             />
           )
         )}
