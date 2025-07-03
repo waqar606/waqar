1098a4d9c27bd1523193dcd2594930193316d570
unknown <waqar.christiano@gmail.com>
2025-07-03 15:27:09 -0500

    One Last Time 6

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -59,7 +59,8 @@ export function ProjectCard({
             loop
             muted
             playsInline
-            className="pointer-events-none mx-auto h-40 w-full object-cover object-top" // needed because random black line at bottom of video
+            // className="pointer-events-none mx-auto h-40 w-full object-cover object-top" // needed because random black line at bottom of video
+            className="w-full h-full object-cover"
           />
         ) : (
           image && (
